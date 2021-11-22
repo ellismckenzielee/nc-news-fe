cd229dfac858070f8d900d351d881b701e4c515b
Ellis Lee <ellismckenzielee@gmail.com>
2021-11-22 15:04:26 +0000

    adds basic grid styling to app.js

---
+++ src/components/ArticleCard.js
@@ -0,0 +1,11 @@
+import "./styles/ArticleCard.css";
+
+const ArticleCard = () => {
+  return (
+    <div className="ArticleCard">
+      <p> Here are some articles! </p>
+    </div>
+  );
+};
+
+export default ArticleCard;
